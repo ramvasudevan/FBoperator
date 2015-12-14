c330002f38bebf2332b8a4d1c0e54c8e1c558931
hoj201 <hoj201@gmail.com>
2015-12-13 22:02:12 -0500

    okay, it is working picture perfect now

--- code/hoj_code/three_torus.py
+++ code/hoj_code/three_torus.py
@@ -1,12 +1,13 @@
 from time import time
 import numpy as np
 from scipy import sparse
-from scipy.sparse.linalg import spsolve
+from scipy.sparse.linalg import LinearOperator
 import scipy.io
-#from scipy.integrate import ode
+from scipy.integrate import ode
+from scipy.integrate import odeint
 import random
 # FOR PLOTTING ANYTHING UNCOMMENT THE FOLLOWING LINE
-from mpl_toolkits.mplot3d import axes3d
+#from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
@@ -32,34 +33,33 @@ dot = lambda m,n: sum( x*y for (x,y) in zip(m,n) )
 # A > B > C > 0 with C << 1 yields chaos
 DIM = 3
 A = 1.0
-B = 0.7
-C = 0.5
+B = 0.5
+C = 0.2
 
 def ABC_flow( arr ,t):
     global A,B,C
-    x = arr[0]
-    y = arr[1]
-    z = arr[2]
-    dx = A*np.sin(z) + C*np.cos(y)
-    dy = B*np.sin(x) + A*np.cos(z)
-    dz = C*np.sin(y) + B*np.cos(x)
-    return np.array([dx,dy,dz])
+    N = len(arr)
+    x = arr[0:N/3]
+    y = arr[N/3:2*N/3]
+    z = arr[2*N/3:N]
+    dx = A*np.sin(2*np.pi*z) + C*np.cos(2*np.pi*y)
+    dy = B*np.sin(2*np.pi*x) + A*np.cos(2*np.pi*z)
+    dz = C*np.sin(2*np.pi*y) + B*np.cos(2*np.pi*x)
+    return np.hstack([dx,dy,dz])
 
 #(u,v,w) = ( Asin(z)+Ccos(y), B sin(x) + A cos(z) , C sin(y) + B cos(x) )
 f_hat = [{},{},{}]
 
-#f_hat[0][(0,0,0)] = 1.0
 f_hat[0][(0, 1,0)] = C*0.5
 f_hat[0][(0,-1,0)] = C*0.5
-f_hat[0][(0,0, 1)] = A*0.5j
-f_hat[0][(0,0,-1)] = -A*0.5j
-#f_hat[1][(0,0,0)] = 1.0
-f_hat[1][( 1,0,0)] = B*0.5j
-f_hat[1][(-1,0,0)] = -B*0.5j
+f_hat[0][(0,0, 1)] = -A*0.5j
+f_hat[0][(0,0,-1)] = A*0.5j
+f_hat[1][( 1,0,0)] = -B*0.5j
+f_hat[1][(-1,0,0)] = B*0.5j
 f_hat[1][(0,0, 1)] = A*0.5
 f_hat[1][(0,0,-1)] = A*0.5
-f_hat[2][(0, 1,0)] = C*0.5j
-f_hat[2][(0,-1,0)] = -C*0.5j
+f_hat[2][(0, 1,0)] = -C*0.5j
+f_hat[2][(0,-1,0)] = C*0.5j
 f_hat[2][( 1,0,0)] = B*0.5
 f_hat[2][(-1,0,0)] = B*0.5
 
@@ -67,54 +67,36 @@ f_hat[2][(-1,0,0)] = B*0.5
 temp = np.linspace(-0.5,0.5,res)
 X_grid,Y_grid,Z_grid= np.meshgrid(temp,temp,temp)
 
-def cayley( A , y ):
-    #outputs cay(A).dot(y)
-    global modes
-    Id = sparse.eye( len(modes), dtype=complex)
-    store = spsolve(Id-A,y)
-    return A.dot(y)+y
-
-def ode_func(t,y):
-    # This is for initializing our integration.
-    global Operator
-    return Operator.dot(y)
-    #return np.zeros( y.shape)
-
-def integrate_point_cloud(n_points,t_span):
-    # initializes a normally distributed point cloud and advects it by the ABC flow
-    points = np.random.randn(n_points,3)*0.25 % 1.0
-    out = np.zeros([ n_points , len(t_span), 3])
-    for point_index in range(0,n_points):
-        y_initial = points[point_index,:]
-        #out[point_index] = ode
-    return 0
-
 def conv_basis(k):
     # Takes in a mode and outputs a convolution matrix associated
     # to the activation of that mode.
     global res
-    out = sparse.eye(res,k=k[0])
+    data = np.ones([2,res])
+    offsets = np.array( [2**p+1, -2**p])
+    Shift = sparse.dia_matrix( (data,offsets) , shape=(res,res),dtype=complex )
+    iShift = Shift.transpose()
+    out = Shift.dot( sparse.eye(res,k=k[0]).dot( iShift) )
     for d in range(1,DIM):
-        e =  sparse.eye(res,k=k[d])
+        e =  Shift.dot( sparse.eye(res,k=k[d]).dot( iShift) )
         store = sparse.kron(out,e)
         out = store.copy()
     return out
 
 def get_conv_mat( g_hat ):
-    # outputs a convolution operator associated to 1D array g_hat
+    # outputs a the (conjugation of) the 
+    # convolution operator associated to 1D array g_hat
     global modes
     N = len(modes)
     out = sparse.dia_matrix((N,N),dtype=complex)
     for k in g_hat.keys():
         e_k = conv_basis(k)
-        out += g_hat[k]*e_k
+        out += g_hat[k].conjugate()*e_k
     return out
 
 def get_translation_generator( dimension_of_translation ):
     # creates the infinitesimal generator along the ath coordinate
     global modes,p,res
     freq = np.fft.fftfreq(res)
-    #I'm not sure why the following is multiplied by res
     ddx = res*sparse.dia_matrix(( 2*np.pi*1j*freq ,
         np.array([0])),
         shape=(res,res) , dtype=complex)
@@ -131,134 +113,125 @@ def get_translation_generator( dimension_of_translation ):
         out = store.copy()
     return out.todia()
 
-def get_Koopman_generator():
-    global modes,f_hat
-    N = len(modes)
-    Op = sparse.dia_matrix( (N,N) , dtype = complex)
-    for d in range(0,DIM):
-        ddx = get_translation_generator(d)
-        f_cnv = get_conv_mat(f_hat[d])
-        Op += f_cnv*ddx
-    return Op.todia()
+ddx = []
+f_cnv = []
+for d in range(3):
+    ddx.append( get_translation_generator(d) )
+    f_cnv.append( get_conv_mat(f_hat[d]) )
 
-def initialize_wave_function():
-    # generates a wave function using fft
-    global modes, res
-    N = len(modes)
-    x_arr = np.linspace(0.0,1.0,res)
-    X = np.meshgrid( x_arr , x_arr , x_arr)
-    sigma = 0.75
-    psi = np.zeros([res,res,res],dtype=complex)
-    # here we construct a wrapped Guassian
-    x_bar = np.array([0.5,0.5,0.5])
-    k_max = np.ceil( 7*sigma )
-    k_list = ( np.array([k1,k2,k3])
-            for k1 in np.arange(-k_max,k_max)
-            for k2 in np.arange(-k_max,k_max)
-            for k3 in np.arange(-k_max,k_max) )
-    for k in k_list:
-        r2 = np.zeros(X[0].shape)
-        for d in range(0,3):
-            r2 += (X[d] - x_bar[d] - k[d])**2
-        psi += np.exp( -r2 / (2.0*sigma**2) )
-    print '%f should be approximately 1.0' % psi.max()
-    psi_hat = np.fft.fftn(psi)
-    return psi_hat.flatten()
+def get_Koopman_Op():
+    def mv(v):
+        global f_cnv,ddx
+        w = np.zeros(v.size,dtype=complex)
+        for d in range(3):
+            w += f_cnv[d].dot( ddx[d].dot(v) )
+        return w
+    return LinearOperator( f_cnv[0].shape , matvec=mv )
 
-def Gaussian_in_Fourier(sigma):
+def get_Frobenius_Perron_Op():
+    def mv(v):
+        global f_cnv,ddx
+        w = np.zeros(v.size,dtype=complex)
+        for d in range(3):
+            w += ddx[d].dot( f_cnv[d].dot(v) )
+        return w
+    return LinearOperator( f_cnv[0].shape , matvec=mv )
+
+def get_Hilbert_Op():
+    def mv(v):
+        global f_cnv,ddx
+        w1 = np.zeros(v.size,dtype=complex)
+        w2 = np.zeros(v.size,dtype=complex)
+        for d in range(3):
+            w1 += f_cnv[d].dot( ddx[d].dot(v) )
+            w2 += ddx[d].dot( f_cnv[d].dot(v) )
+        return 0.5*w1+0.5*w2
+    return LinearOperator( f_cnv[0].shape , matvec=mv )
+
+def Gaussian_in_Fourier(particles=False):
     #generates the Fourier transform of a Guassian
     global modes,res
     N = len(modes)
+    sigma = [ 0.2 , 0.3 , 0.3 ]
     modes_arr = np.array(modes)
+    freq = np.fft.fftfreq(res)
+    unos = np.ones(res)
+    fx = np.einsum('i,j,k->ijk',freq,unos,unos)
+    fy = np.einsum('i,j,k->ijk',unos,freq,unos)
+    fz = np.einsum('i,j,k->ijk',unos,unos,freq)
+    r2 = (fx*sigma[0])**2 + (fy*sigma[1])**2 + (fz*sigma[2])**2
+    out = np.exp( -2*np.pi**2 * r2 * (res**2) )
     print modes_arr.shape
-    return np.exp( -sigma*(res**2*modes_arr**2).sum(1))
+    if particles:
+        N_pts = 15**3
+        X = sigma[0]*np.random.randn(N_pts) / 2#np.sqrt(2)
+        Y = sigma[1]*np.random.randn(N_pts) / 2#np.sqrt(2)
+        Z = sigma[2]*np.random.randn(N_pts) / 2#np.sqrt(2)
+        return out,X%1,Y%1,Z%1
+    return out
 
 #Lets make some operators
 print 'Let\'s make some operators'
 t0=time()
-Koopman_gen = get_Koopman_generator()
-FP_gen = Koopman_gen.transpose().conj().copy()
-Hilbert_gen = 0.5*Koopman_gen -0.5*FP_gen
+Operator = get_Hilbert_Op()
+#Operator = ddx[0]
 print 'Done.  That took me %f seconds.' %(time()-t0)
-
-nnz = Hilbert_gen.nnz
-sparsity = (100.0 * nnz) / ( len(modes)**2)
-print 'There are %e nonzero entries, sparsity = %f %%' %(nnz,sparsity)
-
 print 'initializing psi with p=%d and res=%d' %(p,res)
 t0 = time()
-#psi_hat_initial = initialize_wave_function()
-psi_hat_initial = Gaussian_in_Fourier(0.25)
+psi_hat,X_pts,Y_pts,Z_pts = Gaussian_in_Fourier(particles=True)
 print 'Done.  That took me %f seconds.' %(time()-t0)
 print 'Integrating'
-Operator = Hilbert_gen
-#Operator = get_translation_generator(1) 
+psi = np.fft.ifftn(psi_hat.reshape(res,res,res))
 
-psi = np.fft.ifftn(psi_hat_initial.reshape(res,res,res))
-dpsi_hat = Operator.dot(psi_hat_initial)
-dpsi = np.fft.ifftn(dpsi_hat.reshape(res,res,res))
+#LET'S DISPLAY THE INITIAL CONDITION BEFORE PROCEEDING
+f,ax = plt.subplots(1,2)
+ax[0].imshow( (np.abs(psi)**2).sum(2).transpose() ,
+        cmap='Greys',
+        extent=[0,1,0,1],
+        interpolation='nearest',
+        origin = 'lower')
+ax[0].grid(True)
+ax[0].set_title('GN spectral')
+ax[1].scatter(X_pts%1,Y_pts%1,alpha=0.15,s=3,c='k')
+ax[1].set_aspect('equal')
+ax[1].axis([0,1,0,1])
+ax[1].set_title('Monte Carlo')
+ax[1].grid(True)
+plt.show()
+f.clear()
 
-plt.imshow( psi.real.sum(2) ,
+N_timesteps = 30
+t = np.linspace(0,1,N_timesteps+1 )
+r = ode( lambda t,y: -Operator.dot(y) ).set_integrator('zvode', method='bdf')
+r.set_initial_value( psi_hat.flatten() , t[0] )
+
+
+#INTEGRATING POINT CLOUD
+states = odeint( ABC_flow , np.hstack( [X_pts,Y_pts,Z_pts] ) , t )
+N_pts = states[0].size / 3
+f,ax = plt.subplots(1,2)
+for i in range(N_timesteps):
+    psi = np.fft.ifftn( r.y.reshape(res,res,res) )
+    r.integrate(t[i+1])
+    if r.successful():
+        print 'success'
+    else:
+        print 'fail'
+    ax[0].imshow( (np.abs(psi)**2).sum(2).transpose() ,
             cmap = 'Greys',
             interpolation='nearest',
-            extent = [ 0. , 1. , 0. , 1. ])
-plt.grid(True)
-plt.title("psi")
-plt.show()
-plt.imshow( dpsi.real.sum(2) ,
-            cmap = 'Greys',
-            interpolation='nearest',
-            extent = [ 0. , 1. , 0. , 1. ])
-plt.grid(True)
-plt.title("dpsi")
-plt.show()
-
-y_current = psi_hat_initial.copy()
-dt = 0.005
-t = 0.0
-T = 1.0
-frame_count = 0
-x_arr = np.linspace(0,1,res)
-X,Y = np.meshgrid( x_arr , x_arr )
-while t < T:
-    psi = np.fft.ifftn( y_current.reshape(res,res,res) )
-    k1 = Operator.dot(y_current)
-    k2 = Operator.dot(y_current + dt*k1/2.)
-    k3 = Operator.dot(y_current + dt*k2/2.)
-    k4 = Operator.dot(y_current + dt*k3)
-    y_next = y_current + dt*(k1+2*k2+2*k3+k4)/6.
-    if t > frame_count*T / 10.0:
-        rho = np.abs(psi)**2
-        fname = './density_data/frame_{:,d}'.format(frame_count)
-        np.save(fname,rho)
-        frame_count += 1
-        print 'saved at time {:,.2f}'.format(t)
-    t += dt
-    y_current = y_next.copy()
-   
-
-#ode_instance = ode( ode_func )
-#ode_instance.set_integrator('zvode')
-#t0 = 0.0
-#ode_instance.set_initial_value(psi_hat_initial,t0)
-#ode_instance.set_f_params(Operator)
-#ode_instance.set_jac_params(Operator)
-#t1 = 1.00
-#dt = 0.01
-#fig_num = 0
-#fig = plt.figure()
-#while ode_instance.successful() and ode_instance.t < t1:
-#    ode_instance.integrate( ode_instance.t + dt )
-#    print 'Integrated up to t = %f' %(ode_instance.t)
-#    psi_hat = ode_instance.y
-#    psi = np.fft.ifftn( psi_hat.reshape(res,res,res))
-#    plt.imshow( (np.abs(psi)**2).sum(0) ,
-#            cmap = 'Greys',
-#            interpolation='nearest',
-#            extent = [-0.5,0.5,-0.5,0.5])
-#    plt.grid(True)
-#    plt.title('projection onto yz-plane, t=%.2f' %(ode_instance.t))
-#    plt.savefig('figures/figure_%d.png'%(fig_num))
-#    fig_num += 1
-#    print 'min( rho ) = %f' %(np.min(np.min( np.abs(psi)**2)))
-#    print 'total mass = %f' %(np.sum(np.sum( np.abs(psi)**2)) / (res)**3 )
+            extent = [ 0. , 1. , 0. , 1. ],
+            origin = 'lower')
+    ax[0].grid(True)
+    ax[0].set_title('GN Spectral')
+    X_pts = states[i,0:N_pts]
+    Y_pts = states[i,N_pts:2*N_pts]
+    ax[1].scatter( X_pts % 1 , Y_pts % 1, alpha = .12, s=3., c='k')
+    ax[1].axis([0,1,0,1])
+    ax[1].set_aspect('equal')
+    ax[1].set_title('Monte Carlo')
+    ax[1].grid(True)
+    plt.savefig('./figures/figure_{:,d}'.format(i))
+    ax[0].clear()
+    ax[1].clear()
+    print "print frame %d of %d" %(i,N_timesteps)
